d2553c7705ff88b9c61ee7f1c9c608a7c937f056
Matt Foster <n10914871@qut.edu.au>
2026-05-17 11:56:52 +1000

    added sort by genre

--- website/views.py
+++ website/views.py
@@ -9,11 +9,16 @@ main_bp = Blueprint("main", __name__)
 
 @main_bp.route("/")
 def index():
-    # get the value of p and genre eg. http://127.0.0.1:5000/?genre=electronic, genre = electronic
+    # get the value of p and genre eg. http://127.0.0.1:5000/?genre=Electronic, genre = Electronic
     # if page or genre is not specified, the value will be None
     page = request.args.get("p")
     genre = request.args.get("genre")
 
-    events = Event.query.all()
+    if genre in icons:
+        # events = Event.query.filter(Event.genre == genre).all()
+        events = db.session.execute(db.select(Event).filter_by(genre=genre)).scalars()
+    else:
+        # events = Event.query.all()
+        events = db.session.execute(db.select(Event)).scalars()
 
     return render_template("index.html", events=events, icons=icons)
