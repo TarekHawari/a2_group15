b67fa6a9a43e6ad037ece79ca5e615ca1092cdd3
Matt Foster <n10914871@qut.edu.au>
2026-05-23 12:30:57 +1000

    initial commit

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
 
 from . import db
 from .models import Event
@@ -13,19 +13,32 @@ main_bp = Blueprint("main", __name__)
 
 @main_bp.route("/")
 def index():
-    # get the value of p and genre eg. http://127.0.0.1:5000/?genre=Electronic, genre = Electronic
+    # get the value of p and genre eg. http://127.0.0.1:5000/?genre=Electronic&p=2, genre = Electronic, page = 2
     # if page or genre is not specified, the value will be None
     page = request.args.get("p")
     genre = request.args.get("genre")
 
+    per_page = 3
+
+    # carousel query
     carousels_statement = select(Event).where(Event.status == "Open").order_by(desc("general_admission_available")).limit(3)
     carousels = db.session.scalars(carousels_statement).all()
     carousels_length = len(carousels)
 
-    events_statement = select(Event)
+    # construct main db query
+    events_statement = select(Event).limit(per_page)
     if genre in icons:
         events_statement = events_statement.where(Event.genre == genre)
 
+    if page is not None:
+        try:
+            page = int(page)
+            if page < 1:
+                raise Exception("Page out of bounds")
+        except:
+            return redirect(url_for("main.index"))
+
+    # perform main db query
     events = db.session.scalars(events_statement).all()
     events_length = len(events)
 
