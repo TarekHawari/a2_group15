a8e244190959cf872f268d70d7487f62205d8677
Matt Foster <n10914871@qut.edu.au>
2026-05-23 15:51:25 +1000

    first full pagination implementation

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 
 from . import db
 from .models import Event
@@ -17,8 +17,11 @@ def index():
     # if page or genre is not specified, the value will be None
     page = request.args.get("p")
     genre = request.args.get("genre")
+    if genre is not None and genre not in icons:
+        flash("Invalid genre")
+        return redirect(url_for("main.index", p=page))
 
-    per_page = 3
+    per_page = 1
 
     # carousel query
     carousels_statement = select(Event).where(Event.status == "Open").order_by(desc("general_admission_available")).limit(3)
@@ -27,22 +30,54 @@ def index():
 
     # construct main db query
     events_statement = select(Event).limit(per_page)
-    if genre in icons:
+
+    # add genre to query if there is one
+    if genre is not None:
         events_statement = events_statement.where(Event.genre == genre)
 
+    # make sure page is an int > 0
     if page is not None:
         try:
             page = int(page)
             if page < 1:
-                raise Exception("Page out of bounds")
+                raise Exception()
         except:
-            return redirect(url_for("main.index"))
+            flash("Invalid page")
+            return redirect(url_for("main.index", genre=genre))
+
+    # calculate offset for events statement
+    offset = (page - 1) * per_page if page is not None else 0
+    events_statement = events_statement.offset(offset)
 
     # perform main db query
-    events = db.session.scalars(events_statement).all()
+    current_page_events_statement = events_statement.limit(per_page)
+    events = db.session.scalars(current_page_events_statement).all()
     events_length = len(events)
 
-    # update if events date is in the past
+    # check if page number was invalid
+    if page is not None and page != 1 and len(events) == 0:
+        flash("Invalid page")
+        return redirect(url_for("main.index", genre=genre))
+
+    # check if theres a next page
+    next_page_events_statement = events_statement.limit(per_page * 2)
+    next_page_events = db.session.scalars(next_page_events_statement).all()
+    next_page = False
+    if len(next_page_events) > len(events):
+        next_page = True
+
+    # check if theres a second next page
+    second_next_page = False
+    if page is None or page == 1:
+        second_next_page_events_statement = events_statement.limit(per_page * 3)
+        second_next_page_events = db.session.scalars(second_next_page_events_statement).all()
+        if len(second_next_page_events) > len(next_page_events):
+            second_next_page = True
+
+    # # set page to 1 if it is None because we pass it to index.html
+    page = 1 if page is None else page
+
+    # update if an events date is in the past
     for event in events:
         if event.date < date.today():
             event.status = "Inactive"
@@ -56,4 +91,8 @@ def index():
         carousels=carousels,
         carousels_length=carousels_length,
         events_length=events_length,
+        page=page,
+        next_page=next_page,
+        second_next_page=second_next_page,
+        genre=genre,
     )
